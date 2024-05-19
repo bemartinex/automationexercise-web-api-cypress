45ba83bf3d53d3242dba03121bd41e0987dfed2b
bemartinex <brehmartinez@gmail.com>
2024-05-19 19:07:17 -0300

    Instalacao dependencias Plugin API

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    env:{
+      requestMode: true
+    },
+    fixturesFolder: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
 
